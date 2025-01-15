ac211566578c28c225f8da67449956b0a6839148
Davide Benini <info@davidebenini.it>
2025-01-15 11:49:01 +0100

    Images

---
+++ Sources/UITestsHelper/UITestsHelper+Images.swift
@@ -0,0 +1,22 @@
+//
+//  UITestsHelper+Images.swift
+//  UITestsHelper
+//
+//  Created by Davide Benini on 15/01/25.
+//
+import XCTest
+
+@MainActor
+public extension UITestsHelper {
+    func checkHasImages(count: Int, allowMore: Bool = false) {
+        let images = app.images.allElementsBoundByIndex
+        
+        let tabBarImages = app.tabBars.firstMatch.images.allElementsBoundByIndex
+        let totalCount = images.count - tabBarImages.count
+        if allowMore {
+            XCTAssert(totalCount >= count)
+        } else {
+            XCTAssert(totalCount == count)
+        }
+    }
+}
